fb415d78f87dcf8cb0cf0425a5238097d7ef4db0
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-01-20 07:52:59 +0900

    [add] フォーム追加

--- business_fixedphrase/pages/index.js
+++ business_fixedphrase/pages/index.js
@@ -1,8 +1,9 @@
+import { getDomainLocale } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
 import { useState } from "react";
 
 export default function Home() {
-  const [state, setState] = useState({ lName: "", fName: "" });
+  const [state, setState] = useState({ yourName: "", myName: "", content: "" });
   const firstGreeding = `お世話になっております`;
   const finalGreeding = `以上、よろしくお願いいたします`;
   const handleChange = e => {
@@ -20,23 +21,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>ビジネス文章ジェネレーター</h1>
+      <p>相手の名前:</p>
       <input
-        value={state.lName}
+        value={state.yourName}
         type="text"
         onChange={handleChange}
-        name="lName"
+        name="yourName"
       />
+      <p>自分の名前:</p>
       <input
-        value={state.fName}
+        value={state.myName}
         type="text"
         onChange={handleChange}
-        name="fName"
+        name="myName"
       />
-      <h2>
-        {state.lName},{state.fName}様
-      </h2>
+      <p>メール内容:</p>
+      <input
+        value={state.content}
+        type="text"
+        onChange={handleChange}
+        name="content"
+      />
+      <hr />
+      <h2>{state.yourName}様</h2>
       <h2>{firstGreeding}</h2>
+      <h2>{state.myName}と申します。</h2>
+      <h2>{state.content}</h2>
       <h2>{finalGreeding}</h2>
+      <h2>{state.myName}</h2>
     </div>
   );
 }
