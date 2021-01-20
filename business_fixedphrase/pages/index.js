9c70129fe88a48f49f9dc5555977082f0e7ee449
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-01-20 11:41:55 +0900

    [fix] コンテンツ修正

--- business_fixedphrase/pages/index.js
+++ business_fixedphrase/pages/index.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 
 export default function Home() {
   const [state, setState] = useState({ yourName: "", myName: "", content: "" });
-  const firstGreeding = `お世話になっております`;
-  const finalGreeding = `以上、よろしくお願いいたします`;
+  const firstGreeding = `お世話になっております。`;
+  const finalGreeding = `何卒よろしくお願いいたします。`;
   const handleChange = e => {
     const { name, value } = e.target;
     setState(prevState => ({
@@ -16,39 +16,44 @@ export default function Home() {
 
   return (
     <div className="container">
-      <Head>
-        <title>biz-text-gen</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>ビジネス文章ジェネレーター</h1>
-      <p>相手の名前:</p>
-      <input
-        value={state.yourName}
-        type="text"
-        onChange={handleChange}
-        name="yourName"
-      />
-      <p>自分の名前:</p>
-      <input
-        value={state.myName}
-        type="text"
-        onChange={handleChange}
-        name="myName"
-      />
-      <p>メール内容:</p>
-      <input
-        value={state.content}
-        type="text"
-        onChange={handleChange}
-        name="content"
-      />
-      <hr />
-      <h2>{state.yourName}様</h2>
-      <h2>{firstGreeding}</h2>
-      <h2>{state.myName}と申します。</h2>
-      <h2>{state.content}</h2>
-      <h2>{finalGreeding}</h2>
-      <h2>{state.myName}</h2>
+      <section className="topPageSection">
+        <div className="topPageSection__left">
+          <Head>
+            <title>biz-text-gen</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <h1>ビジネス文章ジェネレーター</h1>
+          <p>相手の名前:</p>
+          <input
+            value={state.yourName}
+            type="text"
+            onChange={handleChange}
+            name="yourName"
+          />
+          <p>自分の名前:</p>
+          <input
+            value={state.myName}
+            type="text"
+            onChange={handleChange}
+            name="myName"
+          />
+          <p>メール内容:</p>
+          <textarea
+            value={state.content}
+            type="text"
+            onChange={handleChange}
+            name="content"
+          ></textarea>
+        </div>
+        <div className="topPageSection__right">
+          <p className="topPageSection__right__text">{state.yourName}様</p>
+          <p className="topPageSection__right__text">{firstGreeding}</p>
+          <p className="topPageSection__right__text">{state.myName}と申します。</p>
+          <p className="topPageSection__right__text">{state.content}</p>
+          <p className="topPageSection__right__text">{finalGreeding}</p>
+          <p className="topPageSection__right__text">{state.myName}</p>
+        </div>
+      </section>
     </div>
   );
 }
